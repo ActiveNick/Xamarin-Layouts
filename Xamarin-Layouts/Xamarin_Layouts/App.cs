213e0c3a697a4384645f4e4bcedb042feb28a7b5
Nick Landry <nlandry@activenick.net>
2016-09-29 14:30:07 -0400

    Initial code commit

---
+++ Xamarin-Layouts/Xamarin_Layouts/App.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Xamarin_Layouts
+{
+	public class App : Xamarin.Forms.Application
+    {
+		public App ()
+		{
+            // The root page of your application
+            MainPage = new GridLayoutXaml();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
